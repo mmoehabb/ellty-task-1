e47974adc6988a1193ce45a0b678ca46196cc9d9
M. E. Abdelsalam <mo.ehab150@gmail.com>
2025-11-10 18:45:57 +0200

    🎉 setup: nextjs framework used, colors palette added, and the desired font installed.

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+// import { PageSelector } from "@/components/forms/PageSelector";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-montserrat">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between pt-21">
+        {/* <PageSelector /> */}
+      </main>
+    </div>
+  );
+}
